5c8fb3f5deb8b2ad4022376665ff3c2b0c55fd9c
Dante Barba <dantebarba@gmail.com>
2018-08-06 20:54:50 -0300

    _API_URL

--- telegram_webserver/biblioteca_comunitaria.py
+++ telegram_webserver/biblioteca_comunitaria.py
@@ -14,14 +14,16 @@ Editorial: %s''' % (self.isbn, self.autor, self.editorial)
 
 class BibliotecaComunitaria:
 
+    _API_URL = "http://pharo:8080/api/"
+
     def isbn(self, isbn=""):
-        response = requests.get("http://pharo:8080/api/"+isbn)
+        response = requests.get(_API_URL+isbn)
         if (response.status_code == 404):
             return "El ISBN ingresado no fue encontrado"
         return self.prettyPrint(response.json())
 
     def books(self):
-        response = requests.get("http://docker.for.mac.localhost:8080/api/books")
+        response = requests.get(_API_URL+"books")
         if (response.status_code == 404):
             return "El ISBN ingresado no fue encontrado"
         return self.prettyPrint(response.json())
