020b89ba8c5358cf06f3e961874502375e832304
Dante Barba <dantebarba@gmail.com>
2018-08-06 20:58:22 -0300

    agregado self.

--- telegram_webserver/biblioteca_comunitaria.py
+++ telegram_webserver/biblioteca_comunitaria.py
@@ -17,13 +17,13 @@ class BibliotecaComunitaria:
     _API_URL = "http://pharo:8080/api/"
 
     def isbn(self, isbn=""):
-        response = requests.get(_API_URL+isbn)
+        response = requests.get(self._API_URL+isbn)
         if (response.status_code == 404):
             return "El ISBN ingresado no fue encontrado"
         return self.prettyPrint(response.json())
 
     def books(self):
-        response = requests.get(_API_URL+"books")
+        response = requests.get(self._API_URL+"books")
         if (response.status_code == 404):
             return "El ISBN ingresado no fue encontrado"
         return self.prettyPrint(response.json())
