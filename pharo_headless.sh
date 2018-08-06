ae09216ce6a80d296b01aa1e55fca551494288b0
Dante Barba <dantebarba@gmail.com>
2018-08-06 20:02:35 -0300

    - pharo image changes

--- pharo_headless.sh
+++ pharo_headless.sh
@@ -1,1 +1,1 @@
-pharo -vm-display-null -vm-sound-null --plugins ./pharo-vm --encoding utf8 ./backup-image/Pharo4.0.image --no-quit
+pharo -vm-display-null -vm-sound-null --encoding utf8 ./backup-image/Pharo4.0.image --no-quit
